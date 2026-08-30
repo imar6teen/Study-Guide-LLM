723b702e7b51df272b51b64614b39ea820bcb8fe
imar6teen <aimaranand02@gmail.com>
2026-08-30 17:52:42 +0700

    fix : make the schema exactly like schema on server

--- frontend/src/types/schema.ts
+++ frontend/src/types/schema.ts
@@ -4,10 +4,10 @@ export const signupSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters long"),
   email: z.email("Invalid email address"),
   username: z.string().min(3, "Username must be at least 3 characters long"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
 export const loginSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters long"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
 });
